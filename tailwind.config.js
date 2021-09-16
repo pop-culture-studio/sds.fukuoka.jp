eb0b6f641c76a56fa3610e1c04d9371e14d7c419
PCS <pcs.engineer.team@gmail.com>
2021-09-16 10:57:47 +0900

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     // mode: 'jit',
     purge: [
         './*.html',
     ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
 
     variants: {
         extend: {
